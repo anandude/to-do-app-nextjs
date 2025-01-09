73f4cbb4cb949c66513408321f75f4da13344628
ananducv <153972540+anandude@users.noreply.github.com>
2025-01-09 10:27:51 +0530

    fixed tailwind config file error using correct import

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
     darkMode: ["class"],
@@ -62,8 +63,8 @@ export default {
 		  minWidth: {
 			'custom': '700px', // your custom min-width
 		  },
-  	}
+	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 
 } satisfies Config;
